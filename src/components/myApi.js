a7c035f185689299e0f5a9e4c3f95135af7c445d
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-10 23:29:18 +0100

    fetchbook from api

--- src/components/myApi.js
+++ src/components/myApi.js
@@ -1,1 +1,9 @@
 const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/mUHyte4brfJIwtKjQbso/books';
+
+export const fetchBookFrmApi = async () => {
+  const response = await fetch(baseUrl, {
+    method: 'GET',
+  });
+  const result = await response.json();
+  return result;
+};
