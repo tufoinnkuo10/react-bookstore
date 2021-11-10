e8ce2236ea7efd4df7c8ab4e2b6ee4b20a385ccc
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-10 23:51:13 +0100

    refactor book link section

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Booksec from './Homepage';
 import Categoriessec from '../Redux/categories/categories';
 import Nav from './NavBar';
 
-const App = () => {
-  const { booksReducer } = useSelector((state) => state);
-  return (
-    <>
-      <Router>
-        <Nav />
-        <Switch>
-          <Route exact path="/">
-            <Booksec data={booksReducer} />
-          </Route>
-          <Route exact path="/categories">
-            <Categoriessec />
-          </Route>
-        </Switch>
-      </Router>
-    </>
-  );
-};
-
+const App = () => (
+  <>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Booksec />
+        </Route>
+        <Route exact path="/categories">
+          <Categoriessec />
+        </Route>
+      </Switch>
+    </Router>
+  </>
+);
 export default App;
