6c84e22a06a185f5db3528fa9b9c5296b6916a50
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-10 11:43:10 +0100

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 const Nav = () => (
   <>
     <nav className="navigation-bar d-flex">
-      <h1>Bookstore</h1>
+      <h1>Tufoins Bookstore</h1>
       <ul className="d-flex">
         <li>
-          <Link to="/" exact> BOOKS </Link>
+          <Link to="/" exact> Books page </Link>
         </li>
         <li>
-          <Link to="/categories"> CATEGORIES </Link>
+          <Link to="/categories"> Categories page </Link>
         </li>
       </ul>
     </nav>
