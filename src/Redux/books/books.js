5336d4f16601fabcec825c02954575aec9738f8e
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-11 08:53:40 +0100

    initialize books and api calls

--- src/Redux/books/books.js
+++ src/Redux/books/books.js
@@ -1,21 +1,45 @@
+import { fetchBookFrmApi, addBookToApi, removeBookFrmApi } from '../../components/myApi';
+
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const INITIALIZE_BOOKS = 'bookStore/books/INITIALIZE_BOOKS';
 const initialState = [];
 
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
-  payload,
+export const addBook = (payload) => (async (dispatch) => {
+  await addBookToApi(payload);
+  dispatch({
+    type: ADD_BOOK,
+    payload,
+  });
 });
-export const removeBook = (payload) => ({
-  type: REMOVE_BOOK,
-  payload,
+export const removeBook = (payload) => (async (dispatch) => {
+  await removeBookFrmApi(payload);
+  dispatch({
+    type: REMOVE_BOOK,
+    payload,
+  });
 });
+export const initializeBooks = () => (async (dispatch) => {
+  const books = await fetchBookFrmApi();
+  const data = Object.entries(books).map(([itemId, [book]]) => ({
+    id: itemId,
+    title: book.title,
+    category: book.category,
+  }));
+  dispatch({
+    type: INITIALIZE_BOOKS,
+    payload: data,
+  });
+});
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload);
+    case INITIALIZE_BOOKS:
+      return [...state, ...action.payload];
     default:
       return state;
   }
