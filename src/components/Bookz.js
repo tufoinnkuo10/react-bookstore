a42a3b518c7a27641279d69518ac07ecbb16aa6e
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-11-12 11:55:40 +0100

    add loader

--- src/components/Bookz.js
+++ src/components/Bookz.js
@@ -1,4 +1,5 @@
 import { useDispatch } from 'react-redux';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { removeBook } from '../Redux/books/books';
 
 const Differentbooks = (prop) => {
@@ -8,10 +9,36 @@ const Differentbooks = (prop) => {
     dispatch(removeBook(id));
   };
   return (
-    <div>
-      {title}
-      {category}
-      <button type="button" onClick={() => removeBookFromStore(id)}>Remove</button>
+    <div className="display-container">
+      <div className="firstpart">
+        <ul className="gotteninfo">
+          <li className="catinput">{category}</li>
+          <li className="titinput">{title}</li>
+          <p className="myname">Author:Suzanne 10en</p>
+        </ul>
+        <div className="lower-remove ">
+          <button className="removebtn" type="button">Comment</button>
+          <p className="breakk">|</p>
+          <button className="removebtn" type="button" onClick={() => removeBookFromStore(id)}>Remove</button>
+          <p className="breakk">|</p>
+          <button className="removebtn" type="button">Edit</button>
+        </div>
+      </div>
+      <div className="loadsec">
+        <span className="loadspin">
+          <AiOutlineLoading3Quarters />
+        </span>
+        <div className="percentage-sec">
+          <p className="percentage">87%</p>
+          <p className="completed">completed</p>
+        </div>
+      </div>
+      <span className="blueline">|</span>
+      <div className="chapter-section">
+        <p className="current-chapter">CURRENT CHAPTER</p>
+        <p className="chapter">chapter10:&#34;lesson learned&#34;</p>
+        <button className="updateprogress" type="button">UPDATE PROGRESS</button>
+      </div>
     </div>
   );
 };
